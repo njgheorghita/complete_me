bf45317463d64400d39325c14b170e1e1e757b92
njgheorghita <njgheorghita@email.wm.edu>
2016-10-19 22:10:39 -0600

    rake fixes

--- rakefile.rb
+++ rakefile.rb
@@ -1,10 +1,22 @@
-require "rake"
-require "rake/testtask"
+require 'rake'
+require 'rake/testtask'
 
-Rake::TestTask.new do |t|
-  t.libs << "test"
-  t.test_files = FileList['test/*_test.rb']
-  t.verbose = true
+Rake::TestTask.new do |test_file|
+  test_file.libs << "test"
+  test_file.test_files = FileList['test/*_test.rb']
+  test_file.verbose = true
+  test_file_array = FileList['test/*_test.rb']
+  puts "
+  ------------- Running test from #{test_file_array[1]}... -------------
+  "
+  ruby test_file_array[1]
+  puts "
+  ------------- Running test from #{test_file_array[2]}... -------------
+  "
+  ruby test_file_array[2]
+  puts "
+  ------------- Running test from #{test_file_array[0]}... This could take a while... -------------
+  "
+  ruby test_file_array[0]
 end
-
-task default: :test # <------ important
+task default: :test
